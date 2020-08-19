065ece5067c9b586f13620c164b71564671ef029
singhaa2 <69884694+singhaa2@users.noreply.github.com>
2020-08-18 23:30:15 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*********************************************************************** 
+ * Hello.java
+ ************************************************************************
+ * Amanpreet Singh
+ * 8/18/2020
+ * 41688 C90
+ ***********************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+	 System.out.println("<Amanpreet Singh>") ;
+	 System.out.println("<Hello.java>") ;
+	 System.out.println("<First program>") ;
+	 System.out.println("<Sublime Text 3.0 9/13/2017>") ;
+	 System.out.println("<41688 C90>") ;
+	 System.out.println() ;
+	 }
+ }
